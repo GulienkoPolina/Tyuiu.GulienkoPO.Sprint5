365ec1880a91f3b27fe8d79fb3f645dec287c750
Polina <gulienko.polina@mail.ru>
2024-11-24 22:55:14 +0500

    Task1

---
+++ Tyuiu.GulienkoPO.Sprint5.Task1.V26/Program.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.GulienkoPO.Sprint5.Task1.V26.Lib;
+
+namespace Tyuiu.GulienkoPO.Sprint5.Task1.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int start = -5, end = 5;
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 2.12x^3 + 1.05x^2 + 4.1x * 2, x=2                                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = ds.SaveToFileTextData(start, end);
+
+            Console.WriteLine("Файл: " + path);
+            Console.WriteLine("Создан");
+            Console.ReadLine();
+        }
+    }
