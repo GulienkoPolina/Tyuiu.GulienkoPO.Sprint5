767777f4325d97864ecd0d0242b7eb2343de00fc
Polina <gulienko.polina@mail.ru>
2024-11-26 18:28:37 +0500

    Task5

---
+++ Tyuiu.GulienkoPO.Sprint5.Task5.V5.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using Tyuiu.GulienkoPO.Sprint5.Task5.V5.Lib;
+
+namespace Tyuiu.GulienkoPO.Sprint5.Task5.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidLoadFromDataFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask5V5.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.GulienkoPO.Sprint5.Task5.V5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using Tyuiu.GulienkoPO.Sprint5.Task5.V5.Lib;
+
+
+namespace Tyuiu.GulienkoPO.Sprint5.Task5.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Найти разницу между максимальным и минимальным целыми числами в файле.  *");
+            Console.WriteLine("* Полученный результат вывести на консоль. У вещественных значений        *");
+            Console.WriteLine("* округлить до трёх знаков после запятой                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = @"C:\DataSprint5\InPutDataFileTask5V5.txt";
+            Console.WriteLine(" Данные находятся в файле: " + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine("Разница между наибольшим и наименьшим значениями: " + res);
+            Console.ReadKey();
+        }
+    }
+}
